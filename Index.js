8d83e56df00b88b018a11bd03ab99da079ef2e80
DanGut19 <111399542+DanGut19@users.noreply.github.com>
2024-04-21 13:17:47 -0600

    Initial commit

---
+++ Index.js
@@ -0,0 +1,19 @@
+var numeros = require("numeros_a_palabras");
+var colors = require("colors");
+
+console.log("Hola Mundo".gray);
+console.log("Convertir Números a Letras".blue);
+console.log(numeros.numero2word(0).Capitalize().toString());
+console.log(numeros.numero2word(100.0).Capitalize().toString());
+console.log(numeros.numero2word(200.0).Capitalize().toString());
+console.log(numeros.numero2word(300.0).Capitalize().toString());
+console.log(numeros.numero2word(400.0).Capitalize().toString());
+console.log(numeros.numero2word(500.0).Capitalize().toString());
+console.log(numeros.numero2word(600.0).Capitalize().toString());
+console.log(numeros.numero2word(700.0).Capitalize().toString());
+console.log(numeros.numero2word(800.0).Capitalize().toString());
+console.log(numeros.numero2word(900.0).Capitalize().toString());
+console.log(numeros.numero2word(1000.0).Capitalize().toString());
+console.log(numeros.numero2word(5550100.0).Capitalize().toString());
+console.log(numeros.numero2word(1500450.04).Capitalize().toString());
+
